9a5897386ce8168811b93296ed34ddc41d23fcdd
xhayper <46065349+xhayper@users.noreply.github.com>
2026-01-09 19:55:50 +0700

    feat: update example

--- examples/example.ts
+++ examples/example.ts
@@ -8,6 +8,12 @@ client.on("message", (data) => {
   switch (data.$type) {
     case "slotData": {
       console.log("Got slot data =>", data.slot.name.value);
+
+      for (const component of data.slot.components) {
+        for (const [key, value] of Object.entries(component.members)) {
+          console.log(`${key} (${value.$type}) => ${value}`);
+        }
+      }
       break;
     }
   }
